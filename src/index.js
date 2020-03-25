f0a85d962f70ea944233e23b3c5e4f807e5b51ba
Robv8r <robv8r@users.noreply.github.com>
2020-03-25 18:42:23 -0500

    Reproducible error.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as css_imp from "./test.module.css";
+import * as scss_imp from "./test.module.scss";
+import * as scss_var_imp from "./test_with_vars.module.scss";
+
+console.log("pure css:\n", css_imp, "\nscss:\n", scss_imp, "\nscss from vars:\n", scss_var_imp);
 
 ReactDOM.render(
   <React.StrictMode>
